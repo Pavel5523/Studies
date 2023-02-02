c4491c0b3b33ae32d032e4217b1f247e84dca1d0
Pavel <ppol.89@mail.ru>
2023-02-02 12:50:38 +0600

    work 02.02.23

--- Homework/homework8/homework8.2.py
+++ Homework/homework8/homework8.2.py
@@ -1,7 +1,17 @@
 from random import randint
 n = int(input('Введите размер списка: '))
 lst = [randint(0, 20) for i in range(n)]
+prime_number = []
+composite_number = []
 print(lst)
-def count(a, b):
-    for i in range(b):
+for i in range(len(lst)):
+    for j in range(2, lst[i] - 1):
+        if lst[i] % j == 0:
+            composite_number.append(lst[i])
+        else:
+            prime_number.append(lst[j])
+print(prime_number)
+print(composite_number)
+
+
         
