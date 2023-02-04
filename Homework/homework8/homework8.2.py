646b27c30dcc5bc3adf3d66d99d931f38faeea4d
Pavel <test@mail.ru>
2023-02-04 10:51:33 +0600

    homework8.2 finish

--- Homework/homework8/homework8.2.py
+++ Homework/homework8/homework8.2.py
@@ -1,23 +1,25 @@
 from random import randint
+
 n = int(input('Введите размер списка: '))
 lst = [randint(0, 20) for i in range(n)]
 prime_number = []
 composite_number = []
-print(lst)
-<<<<<<< HEAD
-def count(a, b):
-    for i in range(b):
-
-=======
+print('Весь список', lst)
 for i in range(len(lst)):
-    for j in range(2, lst[i] - 1):
-        if lst[i] % j == 0:
+    count = 2
+    while count < lst[i]:
+        if lst[i] % count == 0:
             composite_number.append(lst[i])
-        else:
-            prime_number.append(lst[j])
-print(prime_number)
-print(composite_number)
-
-
-        
->>>>>>> refs/remotes/origin/master
+            break
+        if count == lst[i] - 1:
+            prime_number.append(lst[i])
+            break
+        count += 1
+try:
+    print('Минимальное простое число списка =', min(prime_number))
+except ValueError:
+    print('Отсутствует простое число')
+try:
+    print('Максимальное составное число списка =', max(composite_number))
+except ValueError:
+    print('Отсутствует составное число ')
