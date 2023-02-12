75c874edec67adbb7310526b959707fd072787ec
Pavel <test@mail.ru>
2023-02-12 15:53:58 +0600

    homework11 start

---
+++ Homework/homework11/homework10.1.py
@@ -0,0 +1,4 @@
+lst_one = ['red', 'green', 'blue']
+lst_two = ['#FF0000', '#008000', '#0000FF']
+d = dict(zip(lst_one, lst_two))
+print(d)
---
+++ Homework/homework11/homework10.2.py
@@ -0,0 +1,5 @@
+dict_one = {1: 10, 2: 20}
+dict_two = {3: 30, 4: 40}
+dict_three = {5: 50, 6: 60}
+new_dict = dict_one | dict_two | dict_three
+print(new_dict)
