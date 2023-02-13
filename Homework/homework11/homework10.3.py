6457a40d0a75fa47a789995f8808d5e2ed55fa3f
Pavel <test@mail.ru>
2023-02-13 21:31:03 +0600

    finish homework11

--- Homework/homework11/homework10.3.py
+++ Homework/homework11/homework10.3.py
@@ -2,6 +2,11 @@ d = {
     'emp1': {'name': 'Jhon', 'salary': 7500},
     'emp2': {'name': 'Emma', 'salary': 8000},
     'emp3': {'name': 'Brad', 'salary': 6500}
-     }
+}
 for i in d:
-    print(i)
+    print(i, d[i])
+print()
+(d['emp3']['salary']) = int(input('Введите новую зарплату Брэда: '))
+print()
+for i in d:
+    print(i, d[i])
