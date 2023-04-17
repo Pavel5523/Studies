5c69286f288f2271141aedc51625e9597b9e2d6f
Pavel <test@mail.ru>
2023-04-17 21:47:25 +0600

    homework17.04.23

--- Homework/homework29/homework29.py
+++ Homework/homework29/homework29.py
@@ -1,2 +1,9 @@
 class Countries:
-    ...
+    @staticmethod
+    def add_data(**kwargs):
+        with open('file_data.txt', 'w') as f:
+            f.write(**kwargs)
+
+
+a = {'saa': 2}
+Countries.add_data(a)
