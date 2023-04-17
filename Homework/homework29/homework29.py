8422a6506baf0f90039d823a0bc2f4f61ed43be9
Pavel <ppol.89@mail.ru>
2023-04-17 22:58:33 +0600

    homework29.01

--- Homework/homework29/homework29.py
+++ Homework/homework29/homework29.py
@@ -1,9 +1,26 @@
+import json
+
+
 class Countries:
     @staticmethod
-    def add_data(**kwargs):
-        with open('file_data.txt', 'w') as f:
-            f.write(**kwargs)
+    def add_data(b):
+        try:
+            data = json.load(open('file_data.json'))
+        except FileNotFoundError:
+            data = []
+        data.append(b)
+        with open('file_data.json', 'a') as f:
+            json.dump(b, f, indent=2)
 
 
-a = {'saa': 2}
-Countries.add_data(a)
+while True:
+    n = int(input('Введите режим'))
+    if n == 6:
+        break
+    a = input('Введите название страны')
+    b = input('Введите название столицы')
+    d = {
+        'Страна': a,
+        'Столица': b
+    }
+Countries.add_data(d)
