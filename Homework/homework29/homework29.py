fffc7d52ddbfd852e525297c46939d1f9038e649
Pavel <ppol.89@mail.ru>
2023-04-18 15:25:26 +0600

    homework29.02

--- Homework/homework29/homework29.py
+++ Homework/homework29/homework29.py
@@ -2,25 +2,39 @@ import json
 
 
 class Countries:
+    data = []
     @staticmethod
-    def add_data(b):
-        try:
-            data = json.load(open('file_data.json'))
-        except FileNotFoundError:
-            data = []
-        data.append(b)
+    def add_data(c):
+        # try:
+        #     data = json.load(open('file_data.json'))
+        # except FileNotFoundError:
+        #     data = []
+        # data.append(c)
+        # # data = {'Страна': a, 'Столица': b}
+        # with open('file_data.json', 'a') as f:
+        #     json.dump(c, f, indent=2)
+
+
+        # Countries.data = json.load(open('file_data.json'))
+        Countries.data = []
+        Countries.data.append(c)
         with open('file_data.json', 'a') as f:
-            json.dump(b, f, indent=2)
-
-
+            json.dump(c, f, indent=2)
 while True:
-    n = int(input('Введите режим'))
+    n = int(input('Введите режим '))
     if n == 6:
         break
-    a = input('Введите название страны')
-    b = input('Введите название столицы')
+    a = input('Введите название страны ')
+    b = input('Введите название столицы ')
     d = {
         'Страна': a,
         'Столица': b
     }
-Countries.add_data(d)
+    Countries.add_data(d)
+# import json
+# class Countries:
+#     data = []
+#     @staticmethod
+#     def add_data(c):
+#     new_data =
+
