82031abb38c309692b367092dbbccf67e88289d9
Pavel <ppol.89@mail.ru>
2023-04-19 15:26:37 +0600

    homework29.04

--- Homework/homework29/homework29.py
+++ Homework/homework29/homework29.py
@@ -7,11 +7,44 @@ class Countries:
         try:
             data = json.load(open('data.json'))
         except FileNotFoundError:
-            data = []
-        data.append(data_dict)
-        # data = {'name': self.name, 'marks': self.marks}
+            data = {}
+        data.update(data_dict)
         with open('data.json', 'w') as f:
             json.dump(data, f)
+        print('Файл сохранен')
+
+    @staticmethod
+    def del_data(data_del):
+        data = json.load(open('data.json'))
+        try:
+            data.pop(data_del)
+            with open('data.json', 'w') as f:
+                json.dump(data, f)
+        except KeyError:
+            print('Нет такой страны в списке')
+
+
+    @staticmethod
+    def search_data(data_search):
+        data = json.load(open('data.json'))
+        if data.get(data_search):
+            print(data.get(data_search))
+        else:
+            print('Нет такой страны в списке')
+
+    @staticmethod
+    def redaction_data(data_redaction, new_city):
+        data = json.load(open('data.json'))
+        if data_redaction in data.keys():
+        # data[data_redaction] = new_city
+        # with open('data.json', 'w') as f:
+        #     json.dump(data, f)
+        # except KeyError:
+        #     print('Нет такой страны в списке')
+        # print(data.keys())
+    @staticmethod
+    def show_data():
+        print(json.load(open('data.json')))
 
 
 while True:
@@ -22,7 +55,18 @@ while True:
         country = input('Введите название страны ')
         city = input('Введите название столицы ')
         countries = {
-            'Страна': country,
-            'Столица': city
+            country.capitalize(): city.capitalize()
         }
         Countries.add_data(countries)
+    if n == 2:
+        data_del = input('Введите страну для удаления ')
+        Countries.del_data(data_del.capitalize())
+    if n == 3:
+        data_search = input('Введите страну для поиска ')
+        Countries.search_data(data_search.capitalize())
+    if n == 4:
+        data_redaction = input('Введите страну для редактирования ')
+        new_city = input('Введите название новой столицы ')
+        Countries.redaction_data(data_redaction.capitalize(), new_city.capitalize())
+    if n == 5:
+        Countries.show_data()
