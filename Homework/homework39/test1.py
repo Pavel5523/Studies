27877691f541f4343114037657677b3300e3ca80
Pavel <ppol.89@mail.ru>
2023-06-02 15:18:16 +0600

    homework39.1

--- Homework/homework39/test1.py
+++ Homework/homework39/test1.py
@@ -1,23 +1,23 @@
 from jinja2 import Template
 
+info = [{'path':'/index', 'inf':'Главная'},
+        {'path':'/news','inf':'Новости'},
+        {'path':'/about','inf':'О компании'},
+        {'path':'/shop','inf':'Магазин'},
+        {'path':'/contacts','inf':'Контакты'}]
 html = '''
-{%-macro text_input(href, text, class=None)%}
-{%-if class%}
-    <li><a href="{{href}}" class="{{class}}"> {{text}} </a></li>
-{%-else%}
-    <li><a href="{{href}}"> {{text}} </a></li>
-{%-endif%}
-{%-endmacro%}
 <ul>
-    {{-text_input('/index', 'Главная', 'activ')-}}
-    {{-text_input('/news', 'Новости')-}}
-    {{-text_input('/about', 'О компании')-}}
-    {{-text_input('/shop', 'Магазин')-}}
-    {{-text_input('/contacts', 'Контакты')}}
+{%- for i in html %}
+{%-if i.path == '/index'%}
+<li><a href="{{i.path}}" class="active">{{i.inf}}</a></li>
+{%else-%}
+<li><a href="{{i.path}}">{{i.inf}}</a></li>
+{%endif%}
+{%- endfor -%}
 </ul>
 '''
 
 tm = Template(html)
-msg = tm.render(html=html)
+msg = tm.render(html=info)
 
 print(msg)
--- Homework/homework39/test2.py
+++ Homework/homework39/test2.py
@@ -1,14 +1,14 @@
 from jinja2 import Template
 
 html = '''
-{%-macro text_input(type, name, placeholder)%}
+{%-macro text_input(name, placeholder, type='text')%}
     <p><input type="{{type}}" name="{{name}}" placeholder="{{placeholder}}"> </p>
 {%-endmacro%}
-    {{-text_input('text', 'firstname', 'Имя')-}}
-    {{-text_input('text', 'lastname', 'Фамилия')-}}
-    {{-text_input('text', 'address', 'Адрес')-}}
-    {{-text_input('tel', 'phone', 'Телефон')-}}
-    {{-text_input('email', 'email', 'Почта')-}}
+    {{-text_input('firstname', 'Имя')}}
+    {{-text_input('lastname', 'Фамилия')}}
+    {{-text_input('address', 'Адрес')}}
+    {{-text_input('tel', 'phone', 'Телефон')}}
+    {{-text_input('email', 'email', 'Почта')}}
 '''
 
 tm = Template(html)
