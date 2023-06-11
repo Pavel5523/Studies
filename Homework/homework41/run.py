bd595a676b999edb7c816e220ac3aa495227030e
Pavel <ppol.89@mail.ru>
2023-06-11 16:16:12 +0600

    homework41

--- Homework/homework41/run.py
+++ Homework/homework41/run.py
@@ -11,23 +11,24 @@ menu = [
 
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html', title='Главная', menu=menu)
+    return render_template('index.html', title='Главная', menu=menu, prewie='Содержимое главной страницы')
 
 
 @app.route('/second')
 def second():
-    return render_template('second.html', title='Вторая страница', menu=menu)
+    return render_template('second.html', title='Вторая страница', menu=menu, prewie='Содержимое второй страницы')
 
 
 @app.route('/third')
 def third():
-    return render_template('third.html', title='Третья страница', menu=menu)
+    return render_template('third.html', title='Третья страница', menu=menu, prewie='Содержимое третьей страницы')
 
 
 @app.route('/fourth')
 def fourth():
-    return render_template('fourth.html', title='Четвертая страница', menu=menu)
+    return render_template('fourth.html', title='Четвертая страница', menu=menu, prewie='Содержимое четвертой страницы')
 
 
 if __name__ == '__main__':
