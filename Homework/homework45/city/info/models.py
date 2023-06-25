1c25a8f8ecd08ffdecfa409b0c0f12a268ac6d0c
Pavel <ppol.89@mail.ru>
2023-06-25 21:49:16 +0600

    homework45

--- Homework/homework45/city/info/models.py
+++ Homework/homework45/city/info/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class City(models.Model):
     title = models.CharField(max_length=30)
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=1200)
     image = models.ImageField(upload_to='city/images/')
     url = models.URLField()
 
